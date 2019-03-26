7e8fec107f35dd40681118368ff668e0714ff6af
pengxiuxiao <pengxiuxiao@163.com>
2019-03-26 11:01:49 +0800

    创建ips项目框架

---
+++ src/main/java/com/supadata/ips/IpsApplication.java
@@ -0,0 +1,13 @@
+package com.supadata.ips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpsApplication.class, args);
+    }
+
+}
